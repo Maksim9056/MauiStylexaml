005777e5bb74c8835cc77ad04ae1cf71be989aea
Maks <bobreczovm@inbox.ru>
2023-11-27 08:47:13 +0300

    Добавьте файлы проекта.

---
+++ MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace MauiApp3
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Lavel.Style = Set;
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+
+            Lavel.Style = Sets;
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
